845418da60c2dac9ff625a90e74bbf885110b557
Kenryu Shibata <kenryudev5894@gmail.com>
2024-08-11 11:08:54 +0900

    started writting header component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,8 +2,43 @@
 
 <template>
   <div>
-    Component: TheHeader
+    <ul>
+      <li>
+        <NuxtLink to="/news">
+          News
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects">
+          Projects
+        </NuxtLink>
+      </li>
+      <li>Link</li>
+      <li>Contact</li>
+      <li>
+        <NuxtLink to="/about">
+          About SERA
+        </NuxtLink>
+      </li>
+    </ul>
+    <NuxtLink to="/">
+      <img ref="/sera-logo-no-text.svg">
+    </NuxtLink>
+    <ul>
+      <li>
+        <NuxtLink to="/">For non-member</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">Info</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+  display: grid;
+  grid-row: 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>

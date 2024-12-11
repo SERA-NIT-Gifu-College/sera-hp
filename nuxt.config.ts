ca24604c2fd9508a8f07021289cbe5e1e5ff167b
Kenryu Shibata <kenryudev5894@gmail.com>
2024-12-11 23:41:22 +0900

    reviewed nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    baseURL: '/sera-hp/'
+  },
   routeRules: {
     '/': { prerender: true },
     '/about/**': { prerender: true },
@@ -13,6 +16,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   image: {
     inject: true
+  },
+  site: {
+    url: "https://sera-nit-gifu-college.github.io/sera-hp/",
+    name: "SERA Home Page"
   },
   modules: [
     '@nuxt/image',

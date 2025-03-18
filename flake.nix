11cd93f2f5f007a79a92bfcff70fe9e67ac5cac4
Kenryu Shibata <kenryudev5894@gmail.com>
2025-03-18 20:40:54 +0900

    Added nix devshell to this project

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Generic Node.js Template";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pname = "SERA-HP";
+        version = "0.0.1";
+        buildInputs = with pkgs; [
+          nodejs_22
+        ];
+        nativeBuildInputs = buildInputs;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          shellHook = ''
+            export PS1="(devShell ${pname}) \w \$ "
+          '';
+        };
+      }
+    );
+}
